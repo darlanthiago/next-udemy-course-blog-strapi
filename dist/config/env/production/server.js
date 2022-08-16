bc68db62f4bd882c86935adb297fa37a17379e1e
Darlan Thiago <tdarlan1@hotmail.com>
2022-08-15 21:21:55 -0300

    port

--- dist/config/env/production/server.js
+++ dist/config/env/production/server.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
     proxy: true,
     url: env("MY_HEROKU_URL"),
+    port: process.env.PORT,
     app: {
         keys: env.array("APP_KEYS"),
     },
